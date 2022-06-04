8f748a7481a04ae986b71651375532d8b79997e7
Waseem Idelbi <idelbiw@gmail.com>
2022-06-04 00:18:53 -0400

    created the ResultType enum

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,14 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Waseem Idelbi on 6/4/22.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
